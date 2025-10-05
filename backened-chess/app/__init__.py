26b642c6849b95efb6579e806150a6dab4616a7b
joan <joan@kali.joan>
2025-10-05 16:30:39 -0400

    CHESS-16 Migrations

--- backened-chess/app/__init__.py
+++ backened-chess/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from .config import Config
 from .db import db, migrate
-
+from .chess_app import *
+from app.models import *
+from app.routes import *
 
 def create_app():
     app = Flask(__name__)
@@ -12,7 +14,9 @@ def create_app():
     
     # Initialize migrate with both app and db
     migrate.init_app(app, db)
-    
+   # Register blueprints
+    app.register_blueprint(game_bp, url_prefix="/game")
+    app.register_blueprint(moves_bp, url_prefix="/move") 
    
     
     return app
