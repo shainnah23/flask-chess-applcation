94d475c83a41c10e0bb341cb222a14ec82ffaaae
Eric Mbithi <mbithieri@gmail.com>
2025-10-06 09:45:51 +0300

    Updated CHESS-15-user-route

--- backened-chess/app/routes/users.py
+++ backened-chess/app/routes/users.py
@@ -1,49 +1,82 @@
-#  validate and authenticate the user 
-
-from flask import jsonify,Blueprint,request
+from flask import Blueprint,jsonify,request,send_from_directory
 from app.models import Users
 from app.db import db
 import re
+import os
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
 
+bcrypt = Bcrypt()
+
+
+#create student bluprint
 users_bp=Blueprint("users",__name__)
 
 @users_bp.route("/add",methods=["POST"])
 def add_users():
-    print("Add user was hit")
-    data=request.get_json()
+  data=request.get_json()
 
-    name=data.get("name")
-    email=data.get("email")
-    password=data.get("password")
+  name=data.get("name")
+  email=data.get("email")
+  password=data.get("password")
 
-    if not name:
-        return jsonify({"error":"Name is required"}),400
+  if not name:
+    return jsonify({"error":"Name is required"}),400
     
-    if not email:
-        return jsonify({"error":"Email is required"}),400
+  if not email:
+    return jsonify({"error":"Email is required"}),400
 
-    email_regex = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+  email_regex = r"^[\w\.-]+@[\w\.-]+\.\w+$"
 
-    if not re.match(email_regex,email):
-        return jsonify({"error":"Invalid email address"})
-    
-    exists=Users.query.filter_by(email=email).first()
+  if not re.match(email_regex,email):
+    return jsonify({"error":"Invalid email address"})
 
-    if exists:
-        return jsonify({"error":"Email in use"}),400
+  if not password:
+    return jsonify({"error": "Name, email, and password are required"}), 400
 
- 
-    
-    new_user=Users(name=name,email=email,password=password)
-    db.session.add(new_user)
-    db.session.commit()
+  exists=Users.query.filter_by(email=email).first()
 
-    return jsonify({
-        "message":"member added",
-        "student":{
-            "id":new_user.id,
-            "name":new_user.name,
-            "email":new_user.email,
-            "created_at":new_user.created_at
-        }
-    }),201
+  hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+  
+  new_users = Users(name=name, email=email, password=hashed_password)
+  db.session.add(new_users)
+  db.session.commit()
+
+  return jsonify({
+    "message":"Member added successfully",
+    "member":{
+      "id":new_users.id,
+      "name":new_users.name,
+      "email":new_users.email,
+      "created_at":new_users.created_at
+    }
+  })
+
+@users_bp.route("/login",methods=["POST"])
+def login_users():
+  data=request.get_json()
+
+  email=data.get("email")
+  password=data.get("password")
+
+  if not email or not password:
+    return jsonify({"message":"Email and password are required"}),400
+  
+  users=Users.query.filter_by(email=email).first()
+
+  if not users:
+    return jsonify({"message":"user not found"}),401
+  
+  check_password = bcrypt.check_password_hash(users.password, password)
+
+  if not check_password:
+    return jsonify({"message":"Invalid email or password"}),401
+  
+
+  access_token=create_access_token(
+      identity={"id":f"{users.id}","name":users.name},
+      expires_delta=timedelta(seconds=30)
+    )
+
+  return jsonify({ "token":access_token })
