adb75d85c6423ae5f07e6ff802100089d7cf6674
Eric Mbithi <mbithieri@gmail.com>
2025-10-07 13:38:15 +0300

    Adding Required Files for Render

--- backened-chess/main.py
+++ backened-chess/main.py
@@ -10,5 +10,10 @@ CORS(app)
 
 
 
-if __name__=="__main__":
-    app.run(debug=True)
+@app.route("/")
+def home():
+    return "Welcome to our Chess flask API"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
